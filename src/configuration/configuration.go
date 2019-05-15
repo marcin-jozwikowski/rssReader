4ca9e4c5680921e5e60472949325982ecb4941aa
Marcin Jóźwikowski <marcin@jozwikowski.pl>
2019-05-15 20:24:01 +0200

    Dropped struct-ured config in favor of map

--- src/configuration/configuration.go
+++ src/configuration/configuration.go
@@ -7,59 +7,42 @@ import (
 	"os"
 )
 
-type Config struct {
-	Settings []Setting `json:"settings"`
-}
+type Config map[string][]string
 
-type Setting struct {
-	Key    string   `json:"key"`
-	Values []string `json:"values"`
-}
-
-type Raw map[string][]string
-
-func ReadFromFile(filename string) (Config, error) {
+func ReadFromFile(filename string, defaultConfig Config) (Config, error) {
 	if _, err := os.Stat(filename); err == nil {
-		raw, readErr := fromFile(filename)
+		config, readErr := fromFile(filename)
 		if nil != readErr {
 			return Config{}, readErr
 		}
-		return raw.toConfig(), nil
+		return config, nil
 	} else if os.IsNotExist(err) {
-		initializeInFile(filename)
+		fileWriteErr := defaultConfig.WriteToFile(filename)
+		if fileWriteErr != nil {
+			return Config{}, fileWriteErr
+		}
 		return Config{}, fmt.Errorf("Config file created: %v", filename)
 	} else {
 		return Config{}, err
 	}
 }
 
-func fromFile(filename string) (Raw, error) {
-	var raw Raw
+func fromFile(filename string) (Config, error) {
+	var raw Config
 	fileContent, fileReadErr := ioutil.ReadFile(filename)
 	if fileReadErr != nil {
-		return Raw{}, fmt.Errorf("Error while opening file %v: %v", filename, fileReadErr.Error())
+		return Config{}, fmt.Errorf("Error while opening file %v: %v", filename, fileReadErr.Error())
 	} else {
 		var jsonErr = json.Unmarshal(fileContent, &raw)
 		if jsonErr != nil {
-			return Raw{}, fmt.Errorf("Error while parsing file %v: %v", filename, jsonErr.Error())
+			return Config{}, fmt.Errorf("Error while parsing file %v: %v", filename, jsonErr.Error())
 		}
 	}
 	return raw, nil
 }
 
-func (raw Raw) toConfig() Config {
-	var config Config
-	for k, v := range raw {
-		setting := Setting{k,v}
-		config.Settings = append(config.Settings, setting)
-	}
-
-	return config
-}
-
 func (config Config) WriteToFile(filename string) error {
-	raw := config.toRaw()
-	file, err := json.MarshalIndent(raw, "", " ")
+	file, err := json.MarshalIndent(config, "", " ")
 	if err != nil {
 		return fmt.Errorf("Error while encoding Config: %v", err.Error())
 	}
@@ -70,21 +53,4 @@ func (config Config) WriteToFile(filename string) error {
 	}
 
 	return nil
-}
-
-func (config Config) toRaw() Raw {
-	raw := make(Raw, len(config.Settings))
-	for sID := range config.Settings {
-		setting := config.Settings[sID]
-		if len(setting.Key) > 0 {
-			raw[setting.Key] = setting.Values
-		}
-	}
-	return raw
-}
-
-func initializeInFile(filename string) {
-	var conf Config
-	conf.Settings = []Setting{{Key: "tv-shows", Values: []string{"ALF", "That 70's Show"}}}
-	_ = conf.WriteToFile(filename)
 }
