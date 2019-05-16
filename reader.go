91c5dc4f5980fc4d167ffe86d9513ee0f0e7bcae
Marcin Jóźwikowski <marcin@jozwikowski.pl>
2019-05-16 22:41:56 +0200

    Removed default config in favor of editor

--- reader.go
+++ reader.go
@@ -7,25 +7,20 @@ import (
 	"fmt"
 )
 
-const configType_main = "main"
-const configType_cache = "cache"
-
 func main() {
 	configFileName := flag.String("config", "config.json", "Config file location")
 	cacheFileName := flag.String("cacheFile", "cache.json", "Cache file location")
 	runEditor := flag.Bool("runConfig", false, "Run configuration editor")
 	flag.Parse()
 
-	config, configErr := configuration.ReadFromFile(*configFileName, getDefaultConfig(configType_main))
-	cache, cacheErr := configuration.ReadFromFile(*cacheFileName, getDefaultConfig(configType_cache))
-	if configErr != nil || cacheErr != nil {
-		if configErr != nil {
-			fmt.Println(configErr.Error())
-		}
-		if cacheErr != nil {
-			fmt.Println(cacheErr.Error())
-		}
-		return
+	config, configErr := configuration.ReadFromFile(*configFileName)
+	cache, cacheErr := configuration.ReadFromFile(*cacheFileName)
+	if configErr != nil {
+		fmt.Println(configErr.Error())
+		*runEditor = true // enforce config editor
+	}
+	if cacheErr != nil {
+		fmt.Println(cacheErr.Error())
 	}
 
 	if *runEditor {
@@ -36,14 +31,4 @@ func main() {
 
 	cache = feed.Read(config, cache)
 	_ = cache.WriteToFile(*cacheFileName)
-}
-
-func getDefaultConfig(configType string) configuration.Config {
-	var conf = make(configuration.Config,1)
-	switch configType {
-	case configType_main:
-		conf["tv-shows"] = []string{"ALF", "That 70's Show"}
-		break
-	}
-	return conf
 }
