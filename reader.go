aa6b6d61621f67345d20e6d8dc9b26038d436ec3
Marcin Jóźwikowski <marcin@jozwikowski.pl>
2019-05-20 12:15:52 +0200

    Verbose options

--- reader.go
+++ reader.go
@@ -11,16 +11,22 @@ func main() {
 	configFileName := flag.String("configFile", "config.json", "Config file location")
 	cacheFileName := flag.String("cacheFile", "cache.json", "Cache file location")
 	runEditor := flag.Bool("editConfig", false, "Run configuration editor")
+	isVerbose := flag.Int("verbose", configuration.DefaultVerbose, "Verbose level: 0-None ... 3-All")
 	flag.Parse()
 
+	configuration.SetVerbose(*isVerbose)
 	config, configErr := configuration.ReadFromFile(*configFileName)
 	cache, cacheErr := configuration.ReadFromFile(*cacheFileName)
 	if configErr != nil {
-		fmt.Println(configErr.Error())
+		if configuration.IsVerbose() {
+			fmt.Println(configErr.Error())
+		}
 		*runEditor = true // enforce config editor
 	}
 	if cacheErr != nil {
-		fmt.Println(cacheErr.Error())
+		if configuration.IsVerbose() {
+			fmt.Println(cacheErr.Error())
+		}
 	}
 
 	if *runEditor {
