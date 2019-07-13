4a30453cc8df2e200b3cc3e8bce77c53dc5a5eb6
Marcin Jóźwikowski <marcin@jozwikowski.pl>
2019-07-13 13:59:30 +0200

    Removed cacheFile CLI parameter

--- src/cli/flags.go
+++ src/cli/flags.go
@@ -5,7 +5,6 @@ import (
 )
 
 var ConfigFileName *string
-var CacheFileName *string
 var RunEditor *bool
 var Verbose *int
 var Downloader *string
@@ -13,7 +12,6 @@ var DownloaderParams *string
 
 func init() {
 	ConfigFileName = flag.String("configFile", "config.json", "Config file location")
-	CacheFileName = flag.String("cacheFile", "cache.json", "Cache file location")
 	RunEditor = flag.Bool("editConfig", false, "Run configuration editor")
 	Verbose = flag.Int("verbose", DefaultVerbose, "Verbose level: 0-None ... 3-All")
 	Downloader = flag.String("downloader", "surf", "Download command to run. See online documentation for details.")
